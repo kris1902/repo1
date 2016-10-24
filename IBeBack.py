48ccafb22a0fe508f38c682256798c19ff1c20ca
Fraktala <henryk.zulinski@smcebi.edu.pl>
2016-10-24 11:26:19 +0200

    inicjalizacja repozytorium

---
+++ IBeBack.py
@@ -0,0 +1,10 @@
+class IBeBack(Exception):
+##	def __init__(self, added):
+##		self.added=added
+	
+##	def sprawdzenie():
+##		if type(self.added)==list or type(self.added)==tuple:
+##			pass
+##		else:
+##			raise IBeBack
+	pass
---
+++ srednia.py
@@ -0,0 +1,14 @@
+from math import sqrt
+from IBeBack import *
+
+def srednia(lista):
+	if type(lista) != list:
+		raise IBeBack
+	suma=0.0
+	for i in lista:
+		suma+=i**2
+	return sqrt((suma/len(lista)))
+print("proba 1")
+print srednia([1,2,3,4])
+print("proba 2")
+print srednia("ala") 
