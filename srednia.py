b48d2e45108e0aa846a8d8dfd80f8847746885fe
Fraktala <henryk.zulinski@smcebi.edu.pl>
2016-11-14 11:08:58 +0100

    Dodanie średniej arytmetycznej

--- srednia.py
+++ srednia.py
@@ -10,6 +10,17 @@ def srednia(lista):
 	for i in lista:
 		suma+=i**2
 	return sqrt((suma/len(lista)))
+
+def srednia_ar(lista):
+	"srednia arytmetyczna"
+	i=0
+	sum=0
+	for i in range(lista):
+		sum+=i
+		i+=1
+	return sum/i
+
+
 print("proba 1")
 print srednia([1,2,3,4])
 print("proba 2")
