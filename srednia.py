491a7903e75ea437f8fb8603e0fb7e7ff9cff004
Fraktala <henryk.zulinski@smcebi.edu.pl>
2016-11-21 11:24:22 +0100

    Dodanie sprawdzenia danych wejsciowych do programu

--- srednia.py
+++ srednia.py
@@ -1,16 +1,19 @@
 #
 from math import sqrt
 from IBeBack import *
+from tools import *
 
 def srednia(lista):
 	"""isrednia"""
-	if type(lista) != list:
-		raise IBeBack
-	suma=0.0
-	for i in lista:
-		suma+=i**2
-	return sqrt((suma/len(lista)))
-
+	sprawdzenie=sprawdz(lista)
+	if sprawdzenie==True:
+		if type(lista) != list:
+			raise IBeBack
+		suma=0.0
+		for i in lista:
+			suma+=i**2
+		return sqrt((suma/len(lista)))
+	return "Podano zly typ danych wejsciowych"
 
 def srednia_ar(lista):
 	"srednia arytmetyczna"
