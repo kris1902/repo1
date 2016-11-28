e47ec75e3af7366ef0fd305b4ad71ccc3d030af7
Fraktala <henryk.zulinski@smcebi.edu.pl>
2016-11-28 11:08:11 +0100

    Dodalem doc stringi dodalem printa na liscie zebysmy widzieli co sie dzieje zdebugowalem do konca tak ze dziala sprawdzenie na name-main

--- szablon_programu.py
+++ szablon_programu.py
@@ -1,14 +1,17 @@
 class input_tekstu():
+	"""Klasa do pracy na tekscie"""
 	def __init__(self):
 		self.lista=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 	def liczenie(self, nazwa):
+		"""Funkcja do zliaczania slow o danej dlugosci"""
 		dane=open(nazwa+".txt").read()
 		podziel=dane.split(" ")
 		for i in podziel:
 			self.lista[len(i)-1]+=1
+		print self.lista
 		return self.lista
 
 if __name__=="__main__":
 	it=input_tekstu()
-	it.liczenie(plik)
+	it.liczenie("plik")
